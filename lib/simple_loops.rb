fa66f4b1cf0b7e360b745f603fc563ea99edb433
Zachary R Dagnall <zachary.dagnall@gmail.com>
2020-12-03 22:24:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -11,10 +11,16 @@ def loop_message_n_times(mess, n)
   end
 end
 
-def output_array
-  
+def output_array(arr)
+  count = 0 
+  while arr[count] do
+    puts arr[count]
+    count+=1 
+  end
 end
 
-def return_string_array
-  
+def return_string_array(arr)
+  count=0 
+  while arr[count] do
+    
 end
