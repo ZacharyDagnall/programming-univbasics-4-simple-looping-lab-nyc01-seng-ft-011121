9152ebef1f3c3fa9b64c51a73d7f4096c6a02435
Zachary R Dagnall <zachary.dagnall@gmail.com>
2020-12-03 22:22:01 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,20 @@
 # Write your methods here
+def loop_message_five_times(mess)
+  5.times do
+    puts mess 
+  end
+end
+
+def loop_message_n_times(mess, n)
+  n.times do
+    puts mess
+  end
+end
+
+def output_array
+  
+end
+
+def return_string_array
+  
+end
